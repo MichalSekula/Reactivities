ee2034800999b038053044ae6c5182f6d8adab57
ms_rc_sklep <ms_rc_sklep@outlook.com>
2019-08-25 14:12:30 +0200

    Initial commit

---
+++ API/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Zmodyfikowalismy klse ktora odpowiada za uruchomienie calej aplikacji
+            // zmiana polega na tym, iz za kazdym razem kiedy zmienimy cos w naszym Domain
+            // i potrzebujemy zrobic update database, nie musismy tego wykonywac poprzez terminal 
+            // lecz kod ponizej podczas uruchomienia utworzy baze danych jesli nie ma lub zrobi jej update 
+            // o ostatnia migracje.
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
